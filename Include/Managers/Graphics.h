646a7bba97ab5a850d307f6cbeadefca9a34e4c2
Diogo <diogodmaciel450@gmail.com>
2024-08-15 19:33:58 -0300

    gerenciador grafico finalizado ate o momento

--- Include/Managers/Graphics.h
+++ Include/Managers/Graphics.h
@@ -1,1 +1,29 @@
 #pragma once
+
+#include <SFML/Graphics.hpp>
+//incompleto ainda
+namespace Managers {
+
+    class Graphics {
+    private:
+        sf::RenderWindow* window;
+        sf::View view;
+        std::map<const char*, sf::Texture*> textureMap;
+        sf::Clock clock;
+        sf::Font* font;
+        static Managers::Graphics* instance;
+        Graphics();
+    public:
+        ~Graphics();
+        static Graphics* getInstance();
+        void Render(sf::RectangleShape* body); //Pode ser sprite
+        void Render(sf::Text* text);
+        void display();
+        void clear();
+        bool isWindowOpen();
+        void closeWindow();
+        void handleWindowSize();
+        sf::Vector2u getWindowSize() const;
+
+    };
+}
