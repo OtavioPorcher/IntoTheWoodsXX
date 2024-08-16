82788fc6e842ee26d83d3e0dd1447634a5c23dc7
Diogo <diogodmaciel450@gmail.com>
2024-08-16 19:54:29 -0300

    grafico ainda

--- src/Graphics.cpp
+++ src/Graphics.cpp
@@ -1,12 +1,22 @@
-#include "Graphics.h"
+#include "Managers/Graphics.h"
 #define WIDTH 800
 #define HEIGHT 600
 
+using namespace Managers;
+
 Graphics* Graphics::instance = nullptr;
 //incompleto e talvez no lugar errado
+float Graphics::dt = 0;
+
 Graphics::Graphics() {
-    sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "Into the woods++");
+    window = (new sf::RenderWindow(sf::VideoMode(WIDTH, HEIGHT), "Into the woods++"));
 }
 Graphics::~Graphics() {
-    delete();
+    delete(window);
+}
+void Graphics::render(sf::RectangleShape* body) {
+    window->draw(*body);
+}
+void Graphics::render(sf::Text* text) {
+    window->draw(*text);
 }
