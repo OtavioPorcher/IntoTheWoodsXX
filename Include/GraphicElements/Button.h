b7c12c927fd16d7e8121eeffbc70eaab371d3c7c
OtavioPorcher <otavioporcher9@gmail.com>
2024-08-27 13:47:09 -0300

    Botão implementado

---
+++ Include/GraphicElements/Button.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "..\Being.h"
+#include <string>
+
+namespace Menus
+{
+	class Button : public Being
+	{
+	private:
+		sf::Vector2f pos;
+		sf::Text text;
+		sf::Font font;
+	public:
+		Button(sf::Vector2f position, unsigned int pt, std::string txt);
+		~Button();
+
+		void setText(std::string txt);
+
+		void hovering(const bool a);
+
+		void Draw();
+		void Update();
+	};
+}
---
+++ src/Button.cpp
@@ -0,0 +1,50 @@
+#include "..\Include\GraphicElements\Button.h"
+using namespace Menus;
+
+#include <iostream>
+
+
+Button::Button(sf::Vector2f position, unsigned int pt, std::string txt) : Being(bID::button)
+{
+	try { font.loadFromFile("Assets\arialbd.ttf"); }
+	catch (int error)
+	{
+		if (!error)
+		{
+			std::cout << "ERROR: Unable to open font file!" << std::endl;
+		}
+	}
+
+	text.setFont(font);
+	text.setCharacterSize(pt);
+	setText(txt);
+	hovering(false);
+}
+
+Button::~Button()
+{
+
+}
+
+void Button::setText(std::string txt)
+{
+	text.setString(txt);
+}
+
+void Button::hovering(const bool a)
+{
+	if (a)
+		text.setFillColor(sf::Color::Yellow);
+	else
+		text.setFillColor(sf::Color::White);
+}
+
+void Button::Draw()
+{
+	pGM->render(&text);
+}
+
+void Button::Update()
+{
+
+}
